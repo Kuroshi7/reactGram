97a529a9ae1f3cc3a47100c36fd8d9be1fcf35c9
Victor Soffi <victorsoffi@gmail.com>
2024-03-05 20:18:03 -0300

    antes de authGuard(?)

--- backend/controllers/UseController.js
+++ backend/controllers/UseController.js
@@ -12,11 +12,73 @@ const generateToken = (id) => {
 
 //Registar usuario e sign in
 const register = async (req,res) =>{
-    //saida no postman
-    res.send ("Registroo")
+    const {name, email, password} = req.body;
+
+    //check if user exists
+    const user = await User.findOne({email});
+
+    if (user){
+        res.status(422). json ({ errors: ["Por favor, utilize outro e-mail."]});
+        return;
+    }
+
+    //gerar password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //criar usuario
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    //if User was created sucessfully, return the token
+    if (!newUser){
+        res.status(422).json({
+            errors: ["Houve um erro, por favor tente novamente mais tarde."],
+        });
+        return;
+    }
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
+    
 }
+
+//Sing user in
+    const login = async (req,res) => {
+        const {emial, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        //checar se usuario existe
+        if (!user) {
+            res.status(404).json({ errors: ["Usuario não encontrado!"]});
+            return;
+        }
+// checar se senha match
+        if (!(await bcrypt.compare(password, user.password))) {
+            res.status(422).json ({ errors: ["Senha invalida!"]});
+            return;
+        }
+        //retornar usuario com token
+        res.status(200).json({
+            _id: user.id,
+            profileImage: user.profileImage,
+            token: generateToken (user._id),
+        });
+    };
+// Get logged in user
+    const getCurrentUser = async (req, res) => {
+        const user = req.user;
+        res.status (200).json(user);
+    };
 
 // Disponibilizar funçoes para rotas. Imporar arquivo de rotas de forma simples. Exporta como Objeto -{}
 module.exports= {
     register,
+    login,
+    getCurrentUser
 };
--- backend/routes/UserRotas.js
+++ backend/routes/UserRotas.js
@@ -1,21 +1,23 @@
 
 const express = require ("express");
 const router = express.Router();
-
-// Controller
-const{
-    register,} = require ("../controllers/UseController");
+//importando funçoes controller
+const {register, login, getCurrentUser} = require("../controllers/UseController");
 
 //middlewares
 const validate = require ("../middleware/HandleValidation");
 
 //proximo
-const {   userCreateValidation, } = require ("../middleware/userValidacao");
+const {   userCreateValidation, loginValidation } = require ("../middleware/userValidacao");
+
+
 
 
 //routes
 
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+
 
 
 module.exports = router;
