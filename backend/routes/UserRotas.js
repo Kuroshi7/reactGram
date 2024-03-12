9efcac26d9b9b024d51716292414e7ce9a9a05a9
Victor Soffi <victorsoffi@gmail.com>
2024-03-12 14:45:49 -0300

    updated image profile

--- backend/routes/UserRotas.js
+++ backend/routes/UserRotas.js
@@ -7,7 +7,6 @@ const {register, login, getCurrentUser, update} = require("../controllers/UseCon
 //middlewares
 const validate = require ("../middleware/HandleValidation");
 
-//proximo
 const {   userCreateValidation, loginValidation, userUpdateValidation } = require ("../middleware/userValidacao");
 
 const authGuard = require ("../middleware/authGuard");
@@ -19,7 +18,7 @@ const {imageUpload} = require ("../middleware/imageUpload")
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
-router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profiImage"), update);
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update);
 
 
 module.exports = router;
